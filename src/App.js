c8b10a0a456a5da985bc0386a670fc968ba3a14d
Niculina <virtual_n_r@yahoo.com>
2024-05-03 10:03:24 +0300

    deleted spaces

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 let foundWord = document.getElementById('foundWord');
 let notFoundWord = document.getElementById('notFoundWord');
@@ -8,14 +8,15 @@ let toBeAdded = false;
 
 function AddWords(props) {
   const {wordToSearch, doAdd} = props;
-
+  
   function handleClick() {
     doAdd(wordToSearch);
-    console.log('adding word:', wordToSearch);
+    toBeAdded = false;
+    notFoundWord.style.display = "none";
   }
   return (
     <div>
-      <input></input>
+      <input id="inputToAdd" defaultValue={wordToSearch}></input>
       <button onClick={handleClick}>Add word</button>
     </div>
   )
@@ -28,36 +29,36 @@ function Search(props) {
     let wordInput = document.getElementById('wordInput').value.toLocaleLowerCase();
     if (wordInput.length === 0) {
       document.getElementById('wordInput').placeholder = "please enter a word";
-    }
+    };
     if (list.includes(wordInput)) {
       foundWord.style.display = "block";
-      foundWord.style.backgroundColor = "darkgray";
+      foundWord.style.backgroundColor = "lightgray";
       const myWord = React.createElement('h2', {}, 'Last word searched:');
-      const myWord1 = React.createElement('h1', {}, `${wordInput}`);
-      const x = React.createElement('div', {}, [myWord, myWord1]);
-      ReactDOM.render(x, foundWord);
+      const myWordInput = React.createElement('h1', {}, `${wordInput}`);
+      const myDiv = React.createElement('div', {}, [myWord, myWordInput]);
+      const rootDiv = ReactDOM.createRoot(document.getElementById('foundWord'));
+      rootDiv.render(myDiv);
       notFoundWord.style.display = "none";
       toBeAdded = false;
     } else if (!list.includes(wordInput) && (wordInput.length > 0)) {
       notFoundWord.style.display = "block";
       foundWord.style.display = "none";
-      notFoundWord.innerText = 'The above word is not in the dictionary, if you wantt to add it, press "Add word" button';
+      notFoundWord.innerText = 'The above word is not in the dictionary, if you want to add it, press "Add word" button';
       toBeAdded = true;
     } else {
       foundWord.style.display = "none";
       notFoundWord.style.display = "none";
-    }
+    };
 
-    console.log(wordInput);
     doSearch(wordInput);
-
   }
+
   return (
     <div>
       <input id="wordInput"></input>
       <button onClick={handleClick}>Search word</button>
     </div>
-  )
+  );
 }
 
 function Dictionary() {
@@ -66,19 +67,19 @@ function Dictionary() {
 
   function doSearch(value) {
     setWordToSearch(value);
-    console.log('parinte', listItems);
   }
 
   function doAdd(value) {
     setListItems([...listItems, value]);
   }
+
   return (
     <section>
       <h1>Dictionary</h1>
       <Search list={listItems} doSearch={doSearch}/>
       {toBeAdded && <AddWords wordToSearch={wordToSearch} doAdd={doAdd}/>}
     </section>
-  )
+  );
 }
 
 function App() {
