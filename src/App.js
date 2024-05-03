f7f26976434a8bb4036dd5a114ae2d7e70b74b40
Niculina <virtual_n_r@yahoo.com>
2024-05-03 17:42:02 +0300

    changed variables names

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import ReactDOM from 'react-dom/client';
 
-let foundWord = document.getElementById('foundWord');
-let notFoundWord = document.getElementById('notFoundWord');
-foundWord.style.display = "none";
-let toBeAdded = false;
+let wordFound = document.getElementById('wordFound');
+let wordNotFound = document.getElementById('wordNotFound');
+wordFound.style.display = "none";
+let wordsToBeAdded = false;
 
 function AddWords(props) {
-  const {wordToSearch, doAdd} = props;
+  const {wordToSearch, doAddWord} = props;
   
   function handleClick() {
-    doAdd(wordToSearch);
-    toBeAdded = false;
-    notFoundWord.style.display = "none";
+    doAddWord(wordToSearch);
+    wordsToBeAdded = false;
+    wordNotFound.style.display = "none";
   }
   return (
     <div>
@@ -22,35 +22,37 @@ function AddWords(props) {
   )
 }
 
-function Search(props) {
-  const {list, doSearch} = props;
+function SearchWords(props) {
+  const {wordsList, doSearchWords} = props;
 
   function handleClick() {
-    let wordInput = document.getElementById('wordInput').value.toLocaleLowerCase();
+    let wordInput = document.getElementById('wordInput')
+      .value.toLocaleLowerCase();
     if (wordInput.length === 0) {
       document.getElementById('wordInput').placeholder = "please enter a word";
     };
-    if (list.includes(wordInput)) {
-      foundWord.style.display = "block";
-      foundWord.style.backgroundColor = "lightgray";
+    if (wordsList.includes(wordInput)) {
+      wordFound.style.display = "block";
+      wordFound.style.backgroundColor = "lightgray";
       const myWord = React.createElement('h2', {}, 'Last word searched:');
       const myWordInput = React.createElement('h1', {}, `${wordInput}`);
       const myDiv = React.createElement('div', {}, [myWord, myWordInput]);
-      const rootDiv = ReactDOM.createRoot(document.getElementById('foundWord'));
+      const rootDiv = ReactDOM.createRoot(document.getElementById('wordFound'));
       rootDiv.render(myDiv);
-      notFoundWord.style.display = "none";
-      toBeAdded = false;
-    } else if (!list.includes(wordInput) && (wordInput.length > 0)) {
-      notFoundWord.style.display = "block";
-      foundWord.style.display = "none";
-      notFoundWord.innerText = 'The above word is not in the dictionary, if you want to add it, press "Add word" button';
-      toBeAdded = true;
+      wordNotFound.style.display = "none";
+      wordsToBeAdded = false;
+    } else if (!wordsList.includes(wordInput) && (wordInput.length > 0)) {
+      wordNotFound.style.display = "block";
+      wordFound.style.display = "none";
+      wordNotFound.innerText = `The above word is not in the dictionary, 
+        if you want to add it, press "Add word" button`;
+        wordsToBeAdded = true;
     } else {
-      foundWord.style.display = "none";
-      notFoundWord.style.display = "none";
+      wordFound.style.display = "none";
+      wordNotFound.style.display = "none";
     };
 
-    doSearch(wordInput);
+    doSearchWords(wordInput);
   }
 
   return (
@@ -65,19 +67,20 @@ function Dictionary() {
   const [listItems, setListItems] = useState(['word']);
   const [wordToSearch, setWordToSearch] = useState('');
 
-  function doSearch(value) {
+  function doSearchWords(value) {
     setWordToSearch(value);
   }
 
-  function doAdd(value) {
+  function doAddWord(value) {
     setListItems([...listItems, value]);
   }
 
   return (
     <section>
       <h1>Dictionary</h1>
-      <Search list={listItems} doSearch={doSearch}/>
-      {toBeAdded && <AddWords wordToSearch={wordToSearch} doAdd={doAdd}/>}
+      <SearchWords wordsList={listItems} doSearchWords={doSearchWords}/>
+      {wordsToBeAdded && 
+        <AddWords wordToSearch={wordToSearch} doAddWord={doAddWord}/>}
     </section>
   );
 }
