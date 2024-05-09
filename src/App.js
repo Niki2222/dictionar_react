f713688623f793787b589dbc153ffd049593f5a4
Niculina <virtual_n_r@yahoo.com>
2024-05-09 18:50:16 +0300

    all variables included in components

--- src/App.js
+++ src/App.js
@@ -1,115 +1,69 @@
-import React, { useState } from "react";
-import ReactDOM from 'react-dom/client';
+import React, { useState } from 'react';
 
-// let container = document.getElementById('container');
-// let wordFound = document.createElement('div');
-// let wordNotFound = document.createElement('div');
-// wordFound.id = 'wordFound';
-// wordNotFound.id = 'wordNotFound';
-// container.appendChild(wordFound);
-// container.appendChild(wordNotFound);
-// wordFound.style.display = "none";
-
-let wordFound = document.getElementById('wordFound');
-let wordNotFound = document.getElementById('wordNotFound');
-
-let wordsToBeAdded = false;
-
-function Container() {
-  return(
-    <div className="container">
-      <div id="wordFound">F</div>
-      <div id="wordNotFound">N</div>
-    </div>
-  )
-}
-
-function AddWords(props) {
-  const {wordToSearch, doAddWord} = props;
-  
-  function handleClick() {
-    doAddWord(wordToSearch);
-    wordsToBeAdded = false;
-    wordNotFound.style.display = "none";
-  }
+function FoundWord({lastWordSearched}) {
   return (
     <div>
-      <input id="inputToAdd" defaultValue={wordToSearch}></input>
-      <button onClick={handleClick}>Add word</button>
+      <h2>Last word found:</h2>
+      <h1>{lastWordSearched}</h1>
     </div>
-  )
+  );
 }
 
-function SearchWords(props) {
-  const {wordsList, doSearchWords} = props;
-
-  function handleClick() {
-    let wordInput = document.getElementById('wordInput')
-      .value.toLocaleLowerCase();
-    if (wordInput.length === 0) {
-      document.getElementById('wordInput').placeholder = "please enter a word";
-    };
-    if (wordsList.includes(wordInput)) {
-      wordFound.style.display = "block";
-      wordFound.style.backgroundColor = "lightgray";
-      const myWord = React.createElement('h2', {}, 'Last word searched:');
-      const myWordInput = React.createElement('h1', {}, `${wordInput}`);
-      const myDiv = React.createElement('div', {}, [myWord, myWordInput]);
-      const rootDiv = ReactDOM.createRoot(document.getElementById('wordFound'));
-      rootDiv.render(myDiv);
-      wordNotFound.style.display = "none";
-      wordsToBeAdded = false;
-    } else if (!wordsList.includes(wordInput) && (wordInput.length > 0)) {
-      wordNotFound.style.display = "block";
-      wordFound.style.display = "none";
-      wordNotFound.innerText = `The above word is not in the dictionary, 
-        if you want to add it, press "Add word" button`;
-        wordsToBeAdded = true;
-    } else {
-      wordFound.style.display = "none";
-      wordNotFound.style.display = "none";
-    };
-
-    doSearchWords(wordInput);
-  }
-
+function AddWord({lastWordSearched, handleAdd}) {
   return (
     <div>
-      <input id="wordInput"></input>
-      <button onClick={handleClick}>Search word</button>
+      <input type='text' defaultValue={lastWordSearched}></input>
+      <button onClick={handleAdd}>Add word</button>
     </div>
   );
 }
 
 function Dictionary() {
-  const [listItems, setListItems] = useState(['word']);
-  const [wordToSearch, setWordToSearch] = useState('');
+  const [list, setList] = useState(["word"]);
+  const [foundWord, setFoundWord] = useState("");
+  const [lastWordSearched, setLastWordSearched] = useState("")
 
-  function doSearchWords(value) {
-    setWordToSearch(value);
+  function handleClick() {
+    const searchedInput = document.getElementById("inputToSearch").value;
+    if (searchedInput.length === 0) {
+      document.getElementById("inputToSearch").placeholder 
+        = "please enter a word";
+      setFoundWord("");
+    }
+    if (list.includes(searchedInput)) {
+      setFoundWord("found");
+      setLastWordSearched(searchedInput);
+    } else if (!list.includes(searchedInput) && searchedInput.length > 0){
+      setFoundWord("notFound");
+      setLastWordSearched(searchedInput);
+    }
   }
 
-  function doAddWord(value) {
-    setListItems([...listItems, value]);
+  function handleAdd() {
+    setList([...list, lastWordSearched]);
   }
 
   return (
-    <section>
+    <div>
       <h1>Dictionary</h1>
-      <SearchWords wordsList={listItems} doSearchWords={doSearchWords}/>
-      {wordsToBeAdded && 
-        <AddWords wordToSearch={wordToSearch} doAddWord={doAddWord}/>}
-    </section>
-  );
+      <input id='inputToSearch' type='text'></input>
+      <button onClick={handleClick}>Search word</button>
+      {foundWord === "found" && 
+        <FoundWord lastWordSearched={lastWordSearched}/>}
+      {foundWord === "notFound" && 
+        <AddWord lastWordSearched={lastWordSearched} handleAdd={handleAdd}/>}
+    </div>
+  )
 }
 
 function App() {
   return (
-    <>
-      <Dictionary/>
-      <Container />
-    </>
-  )
+    <div className="App">
+      <>
+        <Dictionary />
+      </>
+    </div>
+  );
 }
 
 export default App;
